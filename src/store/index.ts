e976edfee39c9f5964b2fa00fbedc854ae2731f1
cosminailoaie <intern.cosmin.ailoaie@assist.ro>
2020-03-03 11:41:19 +0200

    Initial commit with the new project structure

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import StoreEx from "../store/modules/store";
+import StoreEx from "./modules/store";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
