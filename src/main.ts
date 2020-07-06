2f02eb4e29bd2d0c1c104d72ced8ad675826ae54
cosminailoaie <intern.cosmin.ailoaie@assist.ro>
2020-07-06 16:31:12 +0300

    removed dependency

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,6 @@ Vue.config.productionTip = false;
 Vue.config.performance = true;
 Vue.use(BootstrapVue, VueRouter);
 
-import { Vue2Storage } from 'vue2-storage';
-
-Vue.use(Vue2Storage);
-// You can pass options
-Vue.use(Vue2Storage, {
-  prefix: 'app_',
-  driver: 'local',
-  ttl: 60 * 60 * 24 * 1000, // 24 hours
-});
-
 new Vue({
   router,
   store,
